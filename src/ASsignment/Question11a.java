df5019d86e1fec3b931b36080472795205cba7e0
binit <binitsthapit05@gmail.com>
2021-03-04 20:57:44 -0600

    Added Assignment question 11

---
+++ src/ASsignment/Question11a.java
@@ -0,0 +1,15 @@
+package ASsignment;
+
+public class Question11a {
+    public static void main(String[] args) {
+        int a=2;
+        int b=3;
+        int c=a;
+
+        a=b;
+        b=c;
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
